c7144e95d5ce64470e823197142839c65b7357e2
Kamran Haciyev <68659359+Kamranhaciyev@users.noreply.github.com>
2021-07-18 12:02:40 +0400

    Layihe

---
+++ FinalLayihesi/FinalLayihesi/Controllers/BlogController.cs
@@ -0,0 +1,26 @@
+﻿using FinalLayihesi.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalLayihesi.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly AppDbContext _context;
+        public BlogController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Single()
+        {
+            return View();
+        }
+    }
+}
---
+++ FinalLayihesi/FinalLayihesi/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalLayihesi.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(250), Required]
+        public string Name { get; set; }
+        [MaxLength(250), Required]
+        public string Surname { get; set; }
+        [MaxLength(50), Required]
+        public string Email { get; set; }
+        [MaxLength(250), Required]
+        public string UserName { get; set; }
+        [MaxLength(250)]
+        public string Password { get; set; }
+        public DateTime AddedDate { get; set; }
+        public List<Blog> Blogs { get; set; }
+    }
+}
